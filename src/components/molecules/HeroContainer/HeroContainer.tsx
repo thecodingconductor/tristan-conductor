c6e50543de16bb7ed594ccc38e6e3add377f8b75
Tristan Rais-Sherman <raissherman@work.co>
2021-12-31 16:11:29 -0500

    Basic hero Container functionality working

---
+++ src/components/molecules/HeroContainer/HeroContainer.tsx
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import * as PropTypes from "prop-types";
+import HeadlineHero from "../../atoms/HeadlineHero/HeadlineHero";
+
+const propTypesShape = {
+  heroHeadline: PropTypes.string.isRequired,
+  headerElement: PropTypes.element.isRequired,
+};
+
+type Props = PropTypes.InferProps<typeof propTypesShape>;
+
+const heroContainerStyles = {
+  display: "flex",
+  width: "100%",
+  height: "500px",
+};
+
+const HeroContainer = ({ heroHeadline, headerElement }: Props) => {
+  return (
+    <div
+      sx={{
+        ...heroContainerStyles,
+      }}
+    >
+      {headerElement}
+      <HeadlineHero heroHeadline={heroHeadline} />
+    </div>
+  );
+};
+
+export default HeroContainer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ import { jsx } from "theme-ui";
 import Layout from "../components/Layout";
 import HeadlineHero from "../components/atoms/HeadlineHero/HeadlineHero";
 import { useStaticQuery, graphql } from "gatsby";
+import HeroContainer from "../components/molecules/HeroContainer/HeroContainer";
+
+const headerComponentStyles = {
+  background: "white",
+  fontFamily: "heading",
+};
 
 // markup
 const IndexPage = () => {
@@ -16,13 +22,37 @@ const IndexPage = () => {
     }
   `);
 
-  console.log(data);
-  console.log(data.contentfulSiteCopy.heroHeadline.heroHeadline);
+  const HeaderComponent = () => (
+    <div>
+      <div>
+        <h1
+          sx={{
+            ...headerComponentStyles,
+          }}
+        >
+          Tristan Rais-Sherman,
+        </h1>
+      </div>
+      <div>
+        <h1
+          sx={{
+            ...headerComponentStyles,
+          }}
+        >
+          Conductor
+        </h1>
+      </div>
+    </div>
+  );
 
   return (
     <Layout>
       <HeadlineHero
         heroHeadline={data.contentfulSiteCopy.heroHeadline.heroHeadline}
+      />
+      <HeroContainer
+        heroHeadline={data.contentfulSiteCopy.heroHeadline.heroHeadline}
+        headerElement={<HeaderComponent />}
       />
     </Layout>
   );
