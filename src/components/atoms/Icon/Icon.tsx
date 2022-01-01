8b1c24299080d45efc70316d041aac6902ff8de2
Tristan Rais-Sherman <tristan.raissherman@gmail.com>
2022-01-01 14:37:08 -0500

    Icons (#11)
    * set up icon component a la Vuori
    
    * working with the path tag
    
    * twitter looks good, other icons wonky
    
    * icon component working well
    
    Co-authored-by: Tristan Rais-Sherman <raissherman@work.co>

--- src/components/atoms/Icon/Icon.tsx
+++ src/components/atoms/Icon/Icon.tsx
@@ -3,3 +3,50 @@ import parse, {
   Element,
   HTMLReactParserOptions,
 } from "html-react-parser";
+import * as PropTypes from "prop-types";
+
+import iconData, { iconKeys } from "../../../lib/constants/icons";
+
+const propTypesShape = {
+  className: PropTypes.string,
+  name: PropTypes.oneOf(iconKeys).isRequired,
+};
+
+type IconType = PropTypes.InferProps<typeof propTypesShape>;
+
+const iconStyles = {
+  alignItems: "center",
+  display: "flex",
+  color: "white",
+};
+
+const parseOptions: HTMLReactParserOptions = {
+  replace: (domNode) => {
+    if (domNode instanceof Element && domNode.attribs) {
+      const { attribs, children, name } = domNode;
+      if (name === "svg") {
+        return (
+          <svg aria-hidden={true} {...attribs}>
+            {domToReact(children)}
+          </svg>
+        );
+      }
+    }
+  },
+};
+
+const Icon = ({ className, name }: IconType) => {
+  const icon = iconData[name];
+
+  return <span className={className}>{parse(icon, parseOptions)}</span>;
+};
+
+Icon.propTypes = propTypesShape;
+
+export default Icon;
+
+export const TwitterIcon = (props) => <Icon {...props} name="twitterIcon" />;
+export const TwitchIcon = (props) => <Icon {...props} name="twitchIcon" />;
+export const InstagramIcon = (props) => (
+  <Icon {...props} name="instagramIcon" />
+);
