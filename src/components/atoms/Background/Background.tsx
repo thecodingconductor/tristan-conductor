191bd6384aa6896505db02b8a68d84f16b0ff940
Tristan Rais-Sherman <raissherman@work.co>
2022-01-02 20:28:54 -0500

    forgot absolute positioning

--- src/components/atoms/Background/Background.tsx
+++ src/components/atoms/Background/Background.tsx
@@ -14,7 +14,11 @@ const Background = () => {
     <div
       sx={{
         overflow: "hidden",
-        height: "100%",
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
       }}
     >
       <div
