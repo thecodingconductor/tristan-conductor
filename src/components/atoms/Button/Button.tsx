147d06b58f1accdb12e9c87fae6d6d910395d816
Tristan Rais-Sherman <raissherman@work.co>
2022-01-18 21:32:17 -0500

    build button

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,42 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React from "react";
+import * as PropTypes from "prop-types";
+import { resetButton } from "../../../lib/utils/mixins";
+
+const propTypesShape = {
+  /**
+   * onClick function defines what happens when button is clicked.
+   */
+  onClick: PropTypes.func.isRequired,
+  /**
+   * Text string which appears in button.
+   */
+  label: PropTypes.string.isRequired,
+};
+
+type Props = PropTypes.InferProps<typeof propTypesShape>;
+
+const Button = ({ onClick, label }: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      sx={{
+        ...resetButton,
+        height: "60px",
+        width: "274px",
+        border: "1px solid #FFFFFF",
+      }}
+    >
+      <span
+        sx={{
+          color: "white",
+        }}
+      >
+        {label}
+      </span>
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Button from "../../components/atoms/Button/Button";
 
 const About = () => {
   return (
     <Layout>
-      <div>This is the about page</div>;
+      <div>This is the about page</div>
+      <Button
+        onClick={() => console.log("This is the button")}
+        label={"Learn More"}
+      />
     </Layout>
   );
 };
