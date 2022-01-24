50e39b83ab1b7dcebf21b1e5a3dd72618ff4f2ef
Tristan Rais-Sherman <raissherman@work.co>
2022-01-24 12:20:51 -0500

    state refactor has begun and its going great

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 import React from "react";
 import GlobalState from "./src/context/global/GlobalState";
+import Layout from "./src/components/Layout";
 
 export const wrapRootElement = ({ element }) => {
   return <GlobalState>{element}</GlobalState>;
+};
+
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import React, { useState, useContext, useEffect } from "react";
+import { globalHistory } from "@reach/router";
 import * as PropTypes from "prop-types";
 import Footer from "./Footer";
 import Background from "./atoms/Background/Background";
@@ -16,51 +17,43 @@ const propTypesShape = {
   onClick: PropTypes.func.isRequired,
 };
 
-const Layout = ({ children }) => {
+const Layout = (props) => {
   const { isLarge, isMediumAndBelow } = useBreakpoints();
 
+  const globalContext = useContext(GlobalContext);
+  const { closeMenu, openMenu, isOpen, isSideNavVisible } = globalContext.menu;
+
   return (
-    <GlobalContext.Consumer>
-      {({ gallery, menu }) => {
-        const { isOpen, isSideNavVisible, openMenu, closeMenu } = menu;
-        return (
-          <div
-            sx={{
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <MobileNavIcon
-              closeMenu={closeMenu}
-              openMenu={openMenu}
-              isOpen={isOpen}
-              isSideNavVisible={isSideNavVisible}
-            />
-            <div
-              sx={{
-                height: "inherit",
-                width: "inherit",
-                display: "grid",
-                gridGap: "20px",
-                gridTemplateColumns: [
-                  "repeat(5, 1fr)",
-                  null,
-                  "repeat(12, 1fr)",
-                ],
-                padding: ["0 30px", null, "0 140px"],
-              }}
-            >
-              {children}
-            </div>
-            <Footer />
-
-            <Background />
-
-            <MobileNavOverlay isOpen={isOpen} closeMenu={closeMenu} />
-          </div>
-        );
+    <div
+      sx={{
+        width: "100%",
+        height: "100%",
       }}
-    </GlobalContext.Consumer>
+    >
+      <MobileNavIcon
+        closeMenu={closeMenu}
+        openMenu={openMenu}
+        isOpen={isOpen}
+        isSideNavVisible={isSideNavVisible}
+      />
+      <div
+        sx={{
+          height: "inherit",
+          width: "inherit",
+          display: "grid",
+          gridGap: "20px",
+          gridTemplateColumns: ["repeat(5, 1fr)", null, "repeat(12, 1fr)"],
+          padding: ["0 30px", null, "0 140px"],
+        }}
+      >
+        {props.children}
+      </div>
+      <Footer />
+
+      <Background />
+
+      <MobileNavOverlay isOpen={isOpen} closeMenu={closeMenu} />
+    </div>
   );
 };
 
